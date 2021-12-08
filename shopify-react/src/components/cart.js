a8c191d7d637d1772c2f17e17c44f357325fcea4
tasad <tasad@nexrage.com>
2021-12-07 23:42:16 -0600

    cart styling

--- shopify-react/src/components/cart.js
+++ shopify-react/src/components/cart.js
@@ -12,30 +12,31 @@ import {
     Flex,
     Image,
     Text,
-    Link
+    Link,
+    Box
   } from '@chakra-ui/react'
 import {CloseIcon} from "@chakra-ui/icons"
 import { ShopContext } from "../context/shopContext";
 
 const Cart = () => {
 
-    const { isCartOpen, closeCart, checkout, removeLineItem } = React.useContext(ShopContext)
+  const { isCartOpen, closeCart, checkout, removeLineItem } = React.useContext(ShopContext)
 
-    return (
-   <>
+  return (
+  <>
     <Drawer
         isOpen={isCartOpen}
         placement='right'
         onClose={closeCart}
-      >
+        size="sm"
+    >
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>Your Shopping Cart</DrawerHeader>
 
           <DrawerBody>
-            {
-                checkout.lineItems && checkout.lineItems.map((item) => (
+            {checkout.lineItems?.length ? checkout.lineItems.map((item) => (
                     <Grid templateColumns="repeat(4, 1fr)" gap={1} key={item.id}>
                         <Flex alignItems="center" justifyContent="center">
                             <CloseIcon cursor="pointer" onClick={()=> removeLineItem(item.id)}/>
@@ -50,17 +51,19 @@ const Cart = () => {
                             <Text>{item.variant.price}</Text>
                         </Flex>
                     </Grid>
-                ))
-            }
+                )) : 
+                <Box h="100%" w="100%">
+                  <Text h="100%" display="flex" flexDirection="column" alignItems="center" justifyContent="center" >Your Cart is Empty!</Text>
+                </Box>}
           </DrawerBody>
 
-          <DrawerFooter>
+          {checkout.lineItems?.length ?
+            <DrawerFooter>
             <Button w ="100%"><Link href={checkout.webUrl}>Checkout</Link></Button>
-          </DrawerFooter>
+          </DrawerFooter> : null}
         </DrawerContent>
       </Drawer>
-   </>
-    )
-}
+  </>
+)}
 
 export default Cart
